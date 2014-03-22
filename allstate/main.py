5e6f9b926313c9b25e6f10191fddd15eec17dc07
jjj <jin.yuejenny@gmail.com>
2014-03-22 09:37:49 -0700

    pivot table and car value

--- allstate/main.py
+++ allstate/main.py
@@ -5,8 +5,7 @@ from Kaggle.utilities import RandomForester, print_missing_values_info
 
 from sklearn.preprocessing import Imputer, Normalizer, LabelEncoder
 
-CAR_VALUE_MAPPING = pandas.DataFrame({'car_value': ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', ''],
-                                      'car_vv': [1, 2, 3, 4, 5, 6, 7, 8, 9, np.nan]})
+CAR_VALUE_MAPPING = dict(zip(['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i'], np.arange(1,10)))
 
 
 def pdf(df, head=5):
@@ -28,69 +27,74 @@ def condense_data(origDataFpath, outputFpath, isTraining, verbose=False):
     @param outputFpath: path to the output file, containing the condensed (i.e. one row per customer) data
     @param isTraining: whether the data is training data
     @param verbose: whether to print the first few lines of each table
-    @return: countTable, inputTable, outputTable, condensedTable
+    @return: countTable, inputTable, outputTable, combinedTable
     """
 
-    d = pandas.read_csv(origDataFpath)
-    if verbose: print d.head().to_string()
+    origData = pandas.read_csv(origDataFpath)
+    if verbose:
+        print '\n------- original data ---------'
+        pdf(origData)
 
     # ------------- encode discrete data -----------------
-
     # continuize car value
-    d = pandas.merge(d, CAR_VALUE_MAPPING)
+    origData['car_vv'] = -1
+    for k, v in CAR_VALUE_MAPPING.iteritems():
+        origData.car_vv[origData.car_value == k] = v
 
-    # ------------- input data frames -----------------
-    cols = [u'customer_ID', u'shopping_pt', u'record_type', u'day', u'time', u'state', u'location', u'group_size',
-            u'homeowner', u'car_age', u'car_value', u'car_vv', u'risk_factor', u'age_oldest', u'age_youngest', u'married_couple',
-            u'C_previous', u'duration_previous', u'A', u'B', u'C', u'D', u'E', u'F', u'G', u'cost']
+    if verbose:
+        print '\n------- original data (after car value mapping) ---------'
+        pdf(origData)
+
+    # ------------- consolidate into one row per customer -----------------
+    ind_col = u'customer_ID'
     changing_cols = [u'shopping_pt', u'record_type', u'time', u'state', u'car_value'] # TODO: handle state properly
+    val_cols = [c for c in origData.columns if c != ind_col]     # all columns except customer ID
+    const_cols = [c for c in val_cols if c not in changing_cols]    # columns that are supposedly uniq per customer
+    output_cols = [u'A', u'B', u'C', u'D', u'E', u'F', u'G']
 
-    indColName = u'customer_ID'
-    val_cols = [c for c in cols if c != indColName]     # all columns except customer ID
+    countTable = pandas.pivot_table(origData, rows = ind_col, values=val_cols, aggfunc = lambda vec: len(vec.unique()))[val_cols]
+    if verbose:
+        print '\n------- countTable ---------'
+        pdf(countTable)
 
-    # columns that are supposedly uniq per customer
-    const_cols = [c for c in val_cols if c not in changing_cols]
-
-    # countTable = pandas.pivot_table(d, rows = indColName, values=val_cols, aggfunc = lambda vec: len(vec.unique()))[val_cols]
-    countTable = pandas.pivot_table(d, rows = indColName, values=val_cols, aggfunc = lambda vec: vec.iloc[len(vec)-1])[val_cols]
-    if verbose: print countTable.head().to_string()
-
-    inputTable = pandas.pivot_table(d, rows = indColName, values=const_cols)[const_cols]
-    if verbose: print inputTable.head().to_string()
-
+    inputTable = pandas.pivot_table(origData, rows = ind_col, values=const_cols, aggfunc=lambda vec: np.mean(vec[vec>0]), dropna=False)[const_cols]
+    if verbose:
+        print '\n------- inputTable ---------'
+        pdf(inputTable)
 
     # ------------- output data frame -----------------
     outputTable = None
     if isTraining:
-        output_cols = [u'A', u'B', u'C', u'D', u'E', u'F', u'G']
-        outputTable = d[d.record_type==1][[indColName] + output_cols]
-        outputTable.columns = [indColName] + [c + '_res' for c in output_cols]
-        outputTable = outputTable.set_index(indColName)
-        if verbose: print outputTable.head().to_string()
+        outputTable = origData[origData.record_type==1][[ind_col] + output_cols]
+        outputTable.columns = [ind_col] + [c + '_res' for c in output_cols]
+        outputTable = outputTable.set_index(ind_col)
+        if verbose:
+            print '\n------- outputTable ---------'
+            pdf(outputTable)
 
-        # ------------- combined data frame -----------------
-        condensedTable = inputTable.join(outputTable)
+        combinedTable = inputTable.join(outputTable)
     else:
-        condensedTable = inputTable
+        combinedTable = inputTable
 
-    if verbose: print condensedTable.head().to_string()
+    if verbose:
+        print '\n------- combinedTable ---------'
+        pdf(combinedTable)
 
     # write to file
-    condensedTable.to_csv(outputFpath)
+    combinedTable.to_csv(outputFpath)
 
-    return countTable, inputTable, outputTable, condensedTable
+    return origData, countTable, inputTable, outputTable, combinedTable
 
 fname = 'smallTrain'
 origDataFpath = '/home/jj/code/Kaggle/allstate/Data/' + fname + '.csv'
 outputFpath = '/home/jj/code/Kaggle/allstate/Data/condensed_' + fname + '.csv'
 
-countTable, inputTable, outputTable, condensedTable \
+origData, countTable, inputTable, outputTable, condensedTable \
     = condense_data(origDataFpath, outputFpath, isTraining=True, verbose=False)
 
+pdf(inputTable)
+
 del inputTable[u'location']
-del inputTable[u'car_vv']
-del inputTable[u'homeowner']
-inputTable = inputTable[['A','B','C','D','E','F','G']]
 
 newX = Normalizer().fit_transform(Imputer().fit_transform(inputTable))
 labels = inputTable.columns
