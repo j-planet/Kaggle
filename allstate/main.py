879431a82b1c333d6ae239d71cbe91b4c719478e
root <root@ubuntu.(none)>
2014-04-27 17:04:56 -0700

    preserve individual classifiers

--- allstate/main.py
+++ allstate/main.py
@@ -1,12 +1,13 @@
 import sys
+from pprint import pprint
 sys.path.append('/home/jj/code/Kaggle/allstate')
-from Kaggle.utilities import print_missing_values_info, jjcross_val_score, makePipe, DatasetPair
-from Kaggle.CV_Utilities import fitClfWithGridSearch
 
 from sklearn.preprocessing import Imputer, Normalizer, LabelEncoder
 from sklearn.ensemble import GradientBoostingClassifier, RandomForestClassifier
 from sklearn.metrics import accuracy_score
 
+from Kaggle.utilities import print_missing_values_info, jjcross_val_score, makePipe, DatasetPair
+from Kaggle.CV_Utilities import fitClfWithGridSearch
 from globalVars import *
 from helpers import *
 
@@ -26,7 +27,8 @@ y_train = CombinedClassifier.combine_outputs(np.array(outputTable))
 
 pdf(inputTable)
 
-# ======= validate classifiers =======
+# ======= GRID SEARCH and validate classifiers =======
+
 # plot_feature_importances(X_train, outputTable, inputTable.columns)
 
 #print '--------- combined accuracy score'
@@ -38,6 +40,9 @@ pdf(inputTable)
 
 
 print '----------- individual accuracy score'
+
+indivClfs = []
+
 for col in outputTable.columns:
     cur_y = outputTable[col]
     clf = GradientBoostingClassifier(subsample=0.8, n_estimators=50, learning_rate=0.05)
@@ -57,7 +62,10 @@ for col in outputTable.columns:
                                                 parentsProportion=0.4, mutationProportion=0.1, mutationProbability=0.1,
                                                 mutationStdDev=None, populationSize=4)
 
-    print col, score
+
+    indivClfs.append(newpipe)
+    print '-> ', col, score
+    pprint(bestParams)
     #print col, jjcross_val_score(clf, X_train, cur_y, accuracy_score, cv=5, n_jobs=20).mean()
 
 # ======= predict =======
@@ -70,10 +78,8 @@ X_test = Normalizer().fit_transform(Imputer().fit_transform(inputTable_test))
 
 print '====== TRAINING'
 
-# combinedClf = CombinedClassifier.create_by_cloning(
-#     GradientBoostingClassifier(subsample=0.8, n_estimators=25, learning_rate=0.05), len(OUTPUT_COLS))
-combinedClf = CombinedClassifier.create_by_cloning(
-    RandomForestClassifier(n_estimators=50, n_jobs=20), len(OUTPUT_COLS))
+#combinedClf = CombinedClassifier.create_by_cloning(RandomForestClassifier(n_estimators=50, n_jobs=20), len(OUTPUT_COLS))
+combinedClf = CombinedClassifier(indivClfs)
 combinedClf.fit(X_train, y_train)
 
 print '====== PREDICTING'
@@ -86,4 +92,4 @@ if isValidation:
 # --- write out to file
 res = pandas.DataFrame(index = inputTable_test.index, data = preds)
 res.columns = ['plan']
-res.to_csv('/home/jj/code/Kaggle/allstate/submissions/rf.csv')
+res.to_csv('/home/jj/code/Kaggle/allstate/submissions/rf_indiv.csv')
