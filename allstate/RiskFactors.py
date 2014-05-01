0f6655f4b42b2fc2084cb0310a0eaece38fc5fe1
root <root@ubuntu.(none)>
2014-04-30 19:18:33 -0700

    made imputer into a class

--- allstate/RiskFactors.py
+++ allstate/RiskFactors.py
@@ -6,6 +6,7 @@ sys.path.extend(['/home/jj/code/Kaggle/allstate'])
 
 from sklearn.preprocessing import Imputer, Normalizer
 from sklearn.metrics import auc_score, accuracy_score, precision_score
+from sklearn.base import BaseEstimator, TransformerMixin
 
 from helpers import *
 from globalVars import *
@@ -31,46 +32,91 @@ def impute_risk_factors(inputTable):
     return X_present, y_present, X_missing, ind_missing
 
 
-# ---------- read data ----------
-X_cal, y_cal, _, _ = impute_risk_factors(condense_data('tinyTrain', isTraining=False, readFromFiles=True)[1])
+class ImputerJJ(BaseEstimator, TransformerMixin):
+    """
+    class used for imputation of features with missing values
+    """
 
-# ---------- calibrate to find the best classifier ----------
-bestScore = -1
-bestPipe = None
-bestParams = None
+    def __init__(self, calibrationTable):
+        """
+        calibrate a classifier
+        @param calibrationTable: a pandas data frame
+        """
 
-for name, (pipe, params) in make_pipes().iteritems():
-    print '>'*10, name, '<'*10
-    _, cur_bestParams, cur_bestScore = fitClfWithGridSearch(name + '_risk', pipe, params, DatasetPair(np.array(X_cal), y_cal),
-                                                            saveToDir='/home/jj/code/Kaggle/allstate/output/gridSearchOutput',
-                                                            useJJ=True, score_func=accuracy_score, n_jobs=N_JOBS, verbosity=0,
-                                                            minimize=False, cvSplitNum=5,
-                                                            maxLearningSteps=10,
-                                                            numConvergenceSteps=4, convergenceTolerance=0, eliteProportion=0.1,
-                                                            parentsProportion=0.4, mutationProportion=0.1, mutationProbability=0.1,
-                                                            mutationStdDev=None, populationSize=6)
+        print '--------- Calibrating Imputer -----------'
+        X_cal, y_cal, _, _ = impute_risk_factors(calibrationTable)
 
-    if cur_bestScore > bestScore:
+        bestScore = -1
+        bestPipe = None
+        bestParams = None
 
-        bestScore = cur_bestScore
-        bestPipe = clone(pipe)
-        bestPipe.set_params(**cur_bestParams)
-        bestParams = cur_bestParams
+        for name, (pipe, params) in make_pipes().iteritems():
+            print '>'*10, name, '<'*10
+            _, cur_bestParams, cur_bestScore = fitClfWithGridSearch(name + '_risk', pipe, params, DatasetPair(np.array(X_cal), y_cal),
+                                                                    saveToDir='/home/jj/code/Kaggle/allstate/output/gridSearchOutput',
+                                                                    useJJ=True, score_func=accuracy_score, n_jobs=N_JOBS, verbosity=0,
+                                                                    minimize=False, cvSplitNum=5,
+                                                                    maxLearningSteps=10,
+                                                                    numConvergenceSteps=4, convergenceTolerance=0, eliteProportion=0.1,
+                                                                    parentsProportion=0.4, mutationProportion=0.1, mutationProbability=0.1,
+                                                                    mutationStdDev=None, populationSize=6)
 
-print '----> best score:', bestScore
-pprint(bestParams)
+            if cur_bestScore > bestScore:
 
-# ---------- train the classifier ----------
-X_train, y_train, _, _ = impute_risk_factors(condense_data('tinyTrain', isTraining=False, readFromFiles=True)[1])
-bestPipe.fit(X_train, y_train)
+                bestScore = cur_bestScore
+                bestPipe = clone(pipe)
+                bestPipe.set_params(**cur_bestParams)
+                bestParams = cur_bestParams
 
-# ---------- predict missing risk factors ----------
-inputTable_pred = condense_data('tinyTrain', isTraining=False, readFromFiles=True)[1]
-_, _, X_pred, ind_missing = impute_risk_factors(inputTable_pred)
-y_pred = bestPipe.predict(X_pred)
+        print '----> best score:', bestScore
+        pprint(bestParams)
 
-# ---------- fill in missing risk factors ----------
-inputTable_pred['risk_factor'][ind_missing] = y_pred
+        self._imputer = bestPipe
+
+    def fit(self, trainTable):
+        """
+        train the calibrated classifier
+        @param trainTable: a pandas data frame
+        @return: self
+        """
+
+        X_train, y_train, _, _ = impute_risk_factors(trainTable)
+        self._imputer.fit(X_train, y_train)
+
+        return self
+
+    def transform(self, predTable):
+        """
+        predict missing values using the classifier
+        alteras predTable
+        @return: the altered predTable
+        """
+
+        _, _, X_pred, ind_missing = impute_risk_factors(predTable)
+        y_pred = self._imputer.predict(X_pred)
+
+        predTable['risk_factor'][ind_missing] = y_pred
+
+        return predTable
+
+    def fit_transform(self, X, y=None, **fit_params):
+        """
+        train the calibrated classifier and use it to predict missing values
+        @param X: is in fact trainPredTable
+        """
+
+        trainPredTable = X
+        self.fit(trainPredTable)
+        return self.transform(trainPredTable)
 
 
-print y_pred.shape, X_pred.shape, inputTable_pred['risk_factor'][ind_missing].sum()
+if __name__ == '__main__':
+    imp = ImputerJJ(condense_data('tinyTrain', isTraining=True, readFromFiles=True)[1])
+    imp.fit(condense_data('tinyTrain', isTraining=True, readFromFiles=True)[1])
+
+
+    predTable = condense_data('tinyTrain', isTraining=True, readFromFiles=True)[1]
+    print predTable.risk_factor
+    imp.transform(predTable)
+
+    print predTable.risk_factor
