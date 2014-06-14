b972426309f9e1f4d96f0588f95f43cca00a63f5
root <root@ubuntu.(none)>
2014-06-14 06:45:12 -0700

    finished amend offers code

--- ValuedShoppers/amendOffers.py
+++ ValuedShoppers/amendOffers.py
@@ -20,9 +20,6 @@ doubleFieldDict = {name: {tuple(v): {'avgPrice': 0, 'totalQuantity': 0, 'returnR
 tripleFieldDict = {tuple(v): {'avgPrice': 0, 'totalQuantity': 0, 'returnRate': 0, 'count': 0, 'avgReturnAmt': 0}
                    for v in np.unique(zip(offers.category, offers.company, offers.brand))}
 
-# figure out prices
-offers['offerPrice'] = offers.offervalue / offers.quantity
-
 
 def _process_chunk(chunk, fieldInd, negQuantInd, posQuantInd, res):
     res['count'] += fieldInd.sum()
@@ -96,19 +93,71 @@ def inner_loop(numLinesToRead):
     preprocess_triple_dict(chunk, posQuantInd, negQuantInd)
 
 
-while numLinesLeft > 0:
+def write_singleDict_to_offers():
+    partialNames = singleFieldDict.values()[0].values()[0].keys()
 
+    for fieldName, d in singleFieldDict.iteritems():
+
+        for partialName in partialNames:
+            offers[fieldName + '_' + partialName] = None
+
+        for fieldValue, res in d.iteritems():
+            ind = offers[fieldName] == fieldValue
+
+        for k, v in res.iteritems():
+            offers[fieldName + '_' + k][ind] = v
+
+
+def write_doubleDict_to_offers():
+    partialNames = doubleFieldDict.values()[0].values()[0].keys()
+
+    for fieldNames, d in doubleFieldDict.iteritems():
+
+        for partialName in partialNames:
+            offers['_'.join(list(fieldNames) + [partialName])] = None
+
+        for fieldValues, res in d.iteritems():
+
+            ind = np.logical_and(offers[fieldNames[0]] == fieldValues[0], offers[fieldNames[1]] == fieldValues[1])
+
+            for k, v in res.iteritems():
+                offers['_'.join(list(fieldNames) + [k])][ind] = v
+
+
+def write_tripleDict_to_offers():
+    partialNames = tripleFieldDict.values()[0].keys()
+    fieldName = '_'.join(['category', 'company', 'brand'])
+
+    for partialName in partialNames:
+        offers[fieldName + '_' + partialName] = None
+
+    for fieldValues, res in tripleFieldDict.iteritems():
+
+        ind = np.logical_and(offers['category'] == fieldValues[0], offers['company'] == fieldValues[1], offers['brand'] == fieldValues[2])
+
+        for k, v in res.iteritems():
+            offers[fieldName + '_' + k][ind] = v
+
+
+# figure out prices
+offers['offerPrice'] = offers.offervalue / offers.quantity
+
+# fill the dicts
+while numLinesLeft > 0:
     inner_loop(chunkSize)
     numLinesLeft -= chunkSize
 
-    break
-
-# inner_loop(numLinesLeft)    # the leftover chunk
-
-postprocess_dicts()
-
-pprint(singleFieldDict)
-
+inner_loop(numLinesLeft)    # the leftover chunk
 transactionsFile.close()
 
-# write dicts to offers dataframe
+# post process the dicts
+postprocess_dicts()
+
+# write dicts to the offers data frame
+write_singleDict_to_offers()
+write_doubleDict_to_offers()
+write_tripleDict_to_offers()
+
+
+
+offers.to_csv("/home/jj/code/Kaggle/ValuedShoppers/Data/offers_amended.csv")
