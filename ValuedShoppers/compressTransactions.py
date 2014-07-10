7bd25f70fcadcd4cb54c51ca56890235c204542f
root <root@ubuntu.(none)>
2014-07-10 16:56:16 -0700

    added 180+ fields from MLWave: has_bought times, quantity, amount for the past 7, 30, 60, 180 days and forever

--- ValuedShoppers/compressTransactions.py
+++ ValuedShoppers/compressTransactions.py
@@ -1,13 +1,19 @@
 import pandas
-from dateutil import parser
+
 import numpy as np
 import itertools
+from itertools import chain
 import traceback
 from time import time, sleep
+from dateutil import parser
+from datetime import timedelta
+from os import path
 
 from IterStreamer import IterStreamer
 from Kaggle.pool_JJ import MyPool
 from Kaggle.utilities import runPool, printDoneTime
+
+from globalVars import *
 
 
 def read_transactions_given_id(customerId, transIndexDict, transactionsFile, headers):
@@ -49,6 +55,56 @@ def initStep(*args):
     historyAndOffers, compEmptyDf, transDataDict = args
 
 
+def _processBlockMetrics(transData, ind):
+    """
+    processes a chunk of transaction data
+    @param ind: array of booleans, used for filtering the given transaction data
+    @param transData: block of transaction data (pandas DF)
+    @return: a dictionary
+    """
+
+    numShopped = ind.sum()
+    hasShopped = numShopped > 0
+    freq = 1. * numShopped / transData.shape[0]
+    amt = sum(transData['purchaseamount'][ind])      # total purchase amount
+    quant = sum(transData['purchasequantity'][ind])      # total purchase quantity
+
+    return {'numShopped': numShopped,
+            'neverShopped': not hasShopped,
+            'freq': freq,
+            'hasShopped': hasShopped,
+            'amt': amt,
+            'quant': quant
+    }
+
+
+def _processField(transData, hao, fieldName, ind):
+    """
+    processes a chunk of transaction data
+    @param hao: history and offer data (assumed to be one row)
+    @param transData: block of transaction data (pandas DF)
+    @param ind: array of booleans, used for filtering the given transaction data specific to the field (e.g. category)
+    @return: a dictionary
+    """
+
+    res = {}
+
+    tempD = _processBlockMetrics(transData, ind)     # process block
+    res.update({'_'.join([fieldName, k]): v for k, v in tempD.iteritems()})    # update with block results
+
+    # ----- day-specific fields
+    curTransDates = np.array([parser.parse(d) for d in transData['date']])
+    curOfferDate = parser.parse(hao['offerdate'].irow(0))
+    datesBeforeInd = (curTransDates < curOfferDate)
+
+    for daysGap in [7, 30, 60, 180]:
+        ind_dates = np.logical_and(ind, datesBeforeInd,
+                                   curTransDates >= curOfferDate - timedelta(days=daysGap))
+        tempD = _processBlockMetrics(transData, ind_dates)     # process block
+        res.update({'_'.join([fieldName, k, str(daysGap)]): v for k, v in tempD.iteritems()})    # update with block results
+
+    return res
+
 def innerFunc(args):
 
     global historyAndOffers, compEmptyDf, transDataDict
@@ -63,14 +119,25 @@ def innerFunc(args):
 
             # ---- frequency (has shopped, frequency of shopping at a chain, for example)
             curRow = {'id': customerId}
-            for freqField in freqFields:
 
-                numShopped = (curTransData[freqField] == np.array(hao[freqField])[0]).sum()
-                hasShopped = numShopped > 0
-                freq = 1. * numShopped / curTransData.shape[0]
+            # --- single fields
+            for freqField in freqFields_single:
+                ind = (curTransData[freqField] == np.array(hao[freqField])[0])
+                curRow.update(_processField(curTransData, hao, freqField, ind))
 
-                curRow[freqField + '_freq'] = freq
-                curRow[freqField + '_hasShopped'] = hasShopped
+            # --- double fields
+            for f1, f2 in freqFields_double:
+                ind = np.logical_and(curTransData[f1] == np.array(hao[f1])[0],
+                                     curTransData[f2] == np.array(hao[f2])[0])
+                curRow.update(_processField(curTransData, hao, f1 + '_' + f2, ind))
+
+
+            # --- triple field
+            for f1, f2, f3 in freqFields_triple:    # altho there's only one triplet for now
+                ind = np.logical_and(curTransData[f1] == np.array(hao[f1])[0],
+                                     curTransData[f2] == np.array(hao[f2])[0],
+                                     curTransData[f3] == np.array(hao[f3])[0])
+                curRow.update(_processField(curTransData, hao, f1 + '_' + f2 + '_' + f3, ind))
 
             # ---- time since last shopping date
             dates = [parser.parse(d) for d in curTransData.date]  # convert to datetime objects
@@ -91,25 +158,41 @@ def innerFunc(args):
     return compChunk
 
 
+def flatten(lol):
+    """
+    flattens a list of lists
+    """
+
+    return list(chain.from_iterable(lol))
+
 if __name__ == '__main__':
-    trainHistory = pandas.read_csv("/home/jj/code/Kaggle/ValuedShoppers/Data/trainHistory_wDateFields.csv")
-    offers = pandas.read_csv("/home/jj/code/Kaggle/ValuedShoppers/Data/offers_amended_1.csv")
+    trainHistory = pandas.read_csv(path.join(DATA_DIR, "trainHistory_wDateFields.csv"))
+    offers = pandas.read_csv(path.join(DATA_DIR, "offers.csv"))
     historyAndOffers = pandas.merge(trainHistory, offers, left_on='offer', right_on='offer', how='left')  # join train history and offers
-    transIndexData = pandas.read_csv("/home/jj/code/Kaggle/ValuedShoppers/Data/transIndex.csv")
-    compTransFname = "/home/jj/code/Kaggle/ValuedShoppers/Data/transactions_train_compressed.csv"
+    transIndexData = pandas.read_csv(path.join(DATA_DIR, "transIndex_small.csv"))
+    # compTransFname = path.join(DATA_DIR, "transactions_train_compressed.csv")
+    compTransFname = path.join(DATA_DIR, "temp.csv")
     chunkSize_minor = 250    # number in each process
     chunkSize_major = 4000   # dump every n number of rows
 
-    freqFields = ['chain', 'category', 'company', 'brand']
-    cols = [f + '_freq' for f in freqFields] \
-           + [f + '_hasShopped' for f in freqFields] \
+    freqFields_single = ['chain', 'category', 'company', 'brand']
+    freqFields_double = [('category', 'company'), ('category', 'brand'), ('company', 'brand')]
+    freqFields_triple = [('category', 'company', 'brand')]
+
+    baseFields = ['numShopped', 'freq', 'hasShopped', 'neverShopped', 'amt', 'quant']
+    baseDays = [7, 30, 60, 180]
+    cols = flatten([flatten([ [ '_'.join([f, b, str(d)]) for b in baseFields] for f in freqFields_single]) \
+                    + flatten([ ['_'.join([f1, f2, b, str(d)]) for b in baseFields] for f1, f2 in freqFields_double]) \
+                    + flatten([ ['_'.join([f1, f2, f3, b, str(d)]) for b in baseFields] for f1, f2, f3 in freqFields_triple])
+                    for d in baseDays]) \
            + ['daysSinceLastPch', 'avgWeeklyPchAmt', 'pchAmtWiWeekOfOffer']
+
 
     compEmptyDf = pandas.DataFrame(columns = ['id'] + cols)
     compEmptyDf.to_csv(compTransFname, index=False)     # create new file and write columns to compressed file
     compressedTransFile = open(compTransFname, 'a')     # re-open the outputfile in append mode
 
-    transactionsFile = open("/home/jj/code/Kaggle/ValuedShoppers/Data/transactions.csv")
+    transactionsFile = open(path.join(DATA_DIR, "transactions_small.csv"))
     transHeaders = transactionsFile.readline().strip().split(',')
     blockTransDict = {}
     print '======= NEW MAJOR BLOCK ========'
@@ -124,9 +207,7 @@ if __name__ == '__main__':
         rawData = [transactionsFile.next() for _ in range(numRows)]     # assumes the row numbers in the index file are adjacent
 
         if customerId in np.array(historyAndOffers.id):
-            # print customerId, 'is in train!!'
             blockTransDict[customerId] = pandas.read_csv(IterStreamer(rawData), names = transHeaders)
-            # print len(blockTransDict)
 
         # ---- finished building. run the pool for this major block
         if len(blockTransDict) == chunkSize_major or rowNum == transIndexData.shape[0]-1:
