fc9dd4bfe7758978c7abf6b1d539f1fa16d28462
jennyyuejin <jin.yuejenny@gmail.com>
2014-07-29 21:54:45 -0700

    tiny commit. use 51 fields that explains 85% of variation

--- Fire/main.py
+++ Fire/main.py
@@ -27,19 +27,23 @@ for col in DISCRETE_COLS:
         x_train[col] = x_train[col].replace(to_replace=oldVal, value=newVal)
 
 # plot_histogram(np.array(y_train[y_train > 0]), 25)
+
+# ================== feature selection ==================
+print '================== feature selection =================='
 # print 'about to plot feature importances'
 # plot_feature_importances(x_train, np.array(y_train), x_train.columns, numTopFeatures=0.85, numEstimators=50)
+x_train = x_train[FIELDS_51]
 
-# --------------- train ---------------------
-print '--------------- train ---------------------'
+# ================== train ==================
+print '================== train =================='
 imp = Imputer()
 x_train = imp.fit_transform(np.array(x_train))
 
 clf = GradientBoostingRegressor(loss='quantile', learning_rate=0.05, n_estimators=100, subsample=1)
 clf.fit(np.array(x_train), np.array(y_train))
 
-# --------------- predict ---------------------
-print '--------------- predict ---------------------'
+# ================== predict ==================
+print '================== predict =================='
 testData = pandas.read_csv('/home/jj/code/Kaggle/Fire/Data/test.csv')
 ids_pred = testData['id']
 
@@ -48,4 +52,4 @@ for col in ['id'] + DISCRETE_COLS:
         del testData[col]
 pred = clf.predict(imp.transform(np.array(testData)))
 pandas.DataFrame({'id': ids_pred, 'target': pred}).\
-    to_csv('/home/jj/code/Kaggle/Fire/Submissions/fullTrainInitSubmission.csv', index=False)
+    to_csv('/home/jj/code/Kaggle/Fire/Submissions/fullTrain51FieldsInitSubmission.csv', index=False)
