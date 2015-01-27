92eaf32d135e73f9f714a60815e8733d3e052516
jennyyuejin <jin.yuejenny@gmail.com>
2015-01-26 18:48:56 -0800

    major refactoring. decoupling data reading. WARNING: bugs may exist. much higher loss than before :(

--- NDSB/main.py
+++ NDSB/main.py
@@ -1,12 +1,16 @@
 __author__ = 'jennyyuejin'
 
-#Import libraries for doing image analysis
-from skimage.io import imread
-from skimage.transform import resize
-from sklearn.ensemble import RandomForestClassifier
+import datetime
+import itertools
 import glob
 import os
 from pprint import pprint
+from multiprocessing import cpu_count
+
+from skimage.io import imread
+from skimage.transform import resize
+from sklearn.ensemble import RandomForestClassifier
+
 from sklearn import cross_validation
 from sklearn.cross_validation import StratifiedKFold as KFold
 from sklearn.metrics import classification_report
@@ -21,10 +25,8 @@ import numpy as np
 import pandas
 from scipy import ndimage
 from skimage.feature import peak_local_max
-# make graphics inline
-# %matplotlib inline
 
-DATA_DIR = '/Users/jennyyuejin/K/NDSB/Data'
+from global_vars import DATA_DIR, CLASS_MAPPING, CLASS_NAMES
 
 
 def list_dirs(path):
@@ -98,15 +100,15 @@ def get_minor_major_ratio(_im, plot=False):
 
             plt.show()
     except:
-        return -1
+        return [-1]
 
-    return ratio
+    return [ratio]
 
 
-def read_test_data(testDataDir, maxWidth, maxHeight, featureFuncsNnum=[]):
+def create_test_data_table(testDataDir, maxWidth, maxHeight):
 
     imageSize = maxWidth * maxHeight
-    num_features = imageSize + sum([t[1] for t in featureFuncsNnum])
+    num_features = imageSize
     imgNames = [name for name in os.walk(testDataDir).next()[2] if name[-4:].lower()=='.jpg']
 
     report = [int((j+1)*len(imgNames)/100.) for j in range(100)]
@@ -115,16 +117,8 @@ def read_test_data(testDataDir, maxWidth, maxHeight, featureFuncsNnum=[]):
     for i, imgFname in enumerate(imgNames):
 
         image = imread("{0}{1}{2}".format(testDataDir, os.sep, imgFname), as_grey=True)
-        image = resize(image, (maxWidth, maxHeight))
 
-        # Store the rescaled image pixels and the axis ratio
-        curRow = np.reshape(image, (imageSize, ))
-
-        #  add additional features
-        for func, _ in featureFuncsNnum:
-            curRow = np.append(curRow, func(image))
-
-        X[i, :] = curRow
+        X[i, :] = resize(image, (maxWidth, maxHeight)).reshape((imageSize, ))
 
         # report progress for each 5% done
         if i in report:
@@ -133,13 +127,33 @@ def read_test_data(testDataDir, maxWidth, maxHeight, featureFuncsNnum=[]):
     return X, imgNames
 
 
-def read_training_data(trainDatadir, maxWidth, maxHeight, featureFuncsNnum=[]):
+def append_features(X, imgWidth, imgHeight, featureFuncsNnum):
+    """
+    :param X: numpy array of size imgWidth x imgHeight
+    :param imgWidth:
+    :param imgHeight:
+    :param featureFuncsNnum: [function (returns a list of values), number of features]
+    :return:
+    """
+
+    featureMat = np.zeros((X.shape[0], sum(num for _, num in featureFuncsNnum)))
+
+    for rowInd in range(X.shape[0]):
+
+        img = X[rowInd, :].reshape((imgWidth, imgHeight))
+        featureMat[rowInd, :] = np.array(list(itertools.chain([func(img) for func, _ in featureFuncsNnum])))
+
+    return np.concatenate([X, featureMat], axis=1)
+
+
+def create_training_data_table(trainDatadir, maxWidth, maxHeight):
+
     """
     :param trainDatadir:
     :param maxWidth:
     :param maxHeight:
-    :param featureFuncsNnum: [function (returns a list of values), number of features]
-    :return:
+    :return:    X (numpy array of size numImgs x (maxWidth x maxHeight),
+                y (numpy array of size (numImgs,))
     """
 
     directory_names = list(set(list_dirs(trainDatadir)).difference([trainDatadir]))
@@ -154,26 +168,17 @@ def read_training_data(trainDatadir, maxWidth, maxHeight, featureFuncsNnum=[]):
                 continue
             numberofImages += 1
 
-    # We'll rescale the images
+
     imageSize = maxWidth * maxHeight
     num_rows = numberofImages # one row for each image in the training dataset
-    num_features = imageSize + sum([t[1] for t in featureFuncsNnum])
+    num_cols = imageSize
 
     # X is the feature vector with one row of features per image
     # consisting of the pixel values and our metric
-    X = np.zeros((num_rows, num_features), dtype=float)
-    # y is the numeric class label
-    y = np.zeros(num_rows)
+    X = np.zeros((num_rows, num_cols), dtype=float)
+    y = np.zeros(num_rows)      # y is the numeric class label
 
-    files = []
-
-    # Generate training data
     i = 0
-    label = 0
-
-    # List of string of class names
-    namesClasses = list()
-
     report = [int((j+1)*num_rows/100.) for j in range(100)]
 
     print "Reading images"
@@ -182,49 +187,36 @@ def read_training_data(trainDatadir, maxWidth, maxHeight, featureFuncsNnum=[]):
     for classFolder in directory_names:
 
         # Append the string class name for each class
-        currentClass = classFolder.split(os.sep)[-1]
-        namesClasses.append(currentClass)
+        curClass = classFolder.split(os.sep)[-1].strip()
+        curLabel = CLASS_MAPPING[curClass]
 
-        print 'Class:', currentClass
+        print 'Class:', curClass, curLabel
 
-        hasImg = False
         for imgFname in os.walk(classFolder).next()[2]:
 
             if imgFname[-4:] != ".jpg":
                 continue
 
             # Read in the images and create the features
-            hasImg = True
             nameFileImage = "{0}{1}{2}".format(classFolder, os.sep, imgFname)
             image = imread(nameFileImage, as_grey=True)
-            files.append(nameFileImage)
 
             # print 'Resizing from %ix%i to %ix%i' % (image.shape[0], image.shape[1], maxPixel, maxPixel)
-            image = resize(image, (maxWidth, maxHeight))
-
             # Store the rescaled image pixels and the axis ratio
-            curRow = np.reshape(image, (1, imageSize))
-
-            #  add additional features
-            for func, _ in featureFuncsNnum:
-                curRow = np.append(curRow, func(image))
-                # axisratio = get_minor_major_ratio(image)
-                # X[i, imageSize] = axisratio
-
-            X[i, :] = curRow
+            X[i, :] = np.reshape(resize(image, (maxWidth, maxHeight)), (1, imageSize))
 
             # Store the classlabel
-            y[i] = label
-            i += 1
+            y[i] = curLabel
+
 
             # report progress for each 5% done
             if i in report:
-                print np.ceil(i *100.0 / num_rows), "% done"
+                print np.ceil(i * 100.0 / num_rows), "% done"
 
-        if hasImg:
-            label += 1
+            i += 1
 
-    return X, y, namesClasses, files
+
+    return X, y.astype(int)
 
 
 def plot_ratio_distns_for_pairs(minimumSize=20):
@@ -292,48 +284,93 @@ def multiclass_log_loss(y_true, y_pred, eps=1e-15):
     return loss
 
 
+def write_data_to_files(sizes, outputDir = DATA_DIR):
+    """
+    most likely to be called once only
+    :param sizes: list of sizes. [(width_0, height_0), ..., (width_n, height_n)]
+    :return: file names
+    """
+
+    for width, height in sizes:
+
+        X_train, y = create_training_data_table(os.path.join(DATA_DIR, 'train'), width, height)
+        X_test, testFnames = create_test_data_table(os.path.join(DATA_DIR, 'test'), width, height)
+
+        # train X
+        np.savetxt(os.path.join(outputDir, 'X_train_%i_%i.csv' % (width, height)),
+                   X_train, delimiter=',')
+
+        # train y
+        np.savetxt(os.path.join(outputDir, 'y.csv'), y, delimiter=',')
+
+        # test X
+        pandas.DataFrame(X_test, index=testFnames). \
+            to_csv(os.path.join(outputDir, 'X_test_%i_%i.csv' % (width, height)), header=False)
+
+
+def read_data(width, height, featureFuncsNnum, inputDir = DATA_DIR, isTiny = False):
+    """
+    :param width:
+    :param height:
+    :param featureFuncsNnum:
+    :param inputDir:
+    :param isTiny:
+    :return: x train, y train, x test, xtest filenames
+    """
+
+    print '======= Reading Data ======='
+
+    # ----- read x -----
+    X_train_woFeatures = np.array(pandas.read_csv(os.path.join(
+        inputDir, '%sX_train_%i_%i.csv' % ('tiny' if isTiny else '', width, height)),
+                                                  header=None))
+    X_train = append_features(X_train_woFeatures, width, height, featureFuncsNnum)
+
+    X_test_woFeatures = np.array(pandas.read_csv(
+        os.path.join(inputDir, '%sX_test_%i_%i.csv' % ('tiny' if isTiny else '', width, height)),
+        header=None))
+    X_test = append_features(X_test_woFeatures[:, 1:], width, height, featureFuncsNnum)
+
+    # ----- read y -----
+    y = np.array(pandas.read_csv(os.path.join(inputDir, '%sy.csv' % ('tiny' if isTiny else '')),
+                                 header=None)).flatten()
+
+    print 'DONE reading data. :)'
+
+    return X_train, y.astype(int), X_test, X_test_woFeatures[:, 0]
+
 if __name__ == '__main__':
+    width, height = 25, 25
+    X_train, y, X_test, testFnames = read_data(width, height, [(get_minor_major_ratio, 1)], isTiny=False)
 
-    maxWidth = 25
-    maxLength = 25
-    X_train, y, namesClasses, files = read_training_data(os.path.join(DATA_DIR, 'train'),
-                                                         maxWidth, maxLength, [(get_minor_major_ratio, 1)])
-    X_test, testFnames = read_test_data(os.path.join(DATA_DIR, 'test'),
-                                        maxWidth, maxLength, [(get_minor_major_ratio, 1)])
-
-    np.savetxt(os.path.join(DATA_DIR, 'XwithRatios_%i_%i.csv' % (maxWidth, maxLength)),
-               X_train, delimiter=',')
-    np.savetxt(os.path.join(DATA_DIR, 'y.csv'), y, delimiter=',')
-    pandas.DataFrame(X_test, index=testFnames). \
-        to_csv(os.path.join(DATA_DIR, 'XwithRatios_test_%i_%i.csv' % (maxWidth, maxLength)), header=False)
-
-    print "CV-ing"
-    clf = RandomForestClassifier(n_estimators=100, n_jobs=7)
-    scores = cross_validation.cross_val_score(clf, X_train, y, cv=5, n_jobs=7)
-
-    print "Accuracy of all classes"
-    print np.mean(scores)
+    # print "CV-ing"
+    # scores = cross_validation.cross_val_score(RandomForestClassifier(n_estimators=100, n_jobs=cpu_count()-1),
+    #                                           X_train, y, cv=5, n_jobs=cpu_count()-1)
+    #
+    # print "Accuracy of all classes:", np.mean(scores)
 
     # Get the probability predictions for computing the log-loss function
     # prediction probabilities number of samples, by number of classes
     y_pred = y * 0
-    y_pred_mat = np.zeros((len(y), len(set(y))))
+    y_pred_mat = np.zeros((len(y), len(CLASS_NAMES)))   # forcing all class names, for testing with partial data
 
     for trainInd, testInd in KFold(y, n_folds=5):
-        clf = RandomForestClassifier(n_estimators=100, n_jobs=3)
+        clf = RandomForestClassifier(n_estimators=100, n_jobs=cpu_count()-1)
         clf.fit(X_train[trainInd, :], y[trainInd])
 
         y_pred[testInd] = clf.predict(X_train[testInd, :])
-        y_pred_mat[testInd] = clf.predict_proba(X_train[testInd, :])
+        y_pred_mat[testInd, :][:, np.sort(list(set(y)))] = clf.predict_proba(X_train[testInd, :])
 
     print '>>>>>> Classification Report'
-    print classification_report(y, y_pred, target_names=namesClasses)
+    print classification_report(y, y_pred, target_names=CLASS_NAMES)
 
     print '\n>>>>>>>Multi-class Log Loss =', multiclass_log_loss(y, y_pred_mat)
 
     # make predictions and write to file
-    clf = RandomForestClassifier(n_estimators=100, n_jobs=7)
+    clf = RandomForestClassifier(n_estimators=100, n_jobs=cpu_count()-1)
     clf.fit(X_train, y)
-    y_test_pred = clf.predict_proba(X_test)
-    pandas.DataFrame(y_test_pred, columns=namesClasses, index=testFnames) \
-        .to_csv(os.path.join(DATA_DIR, 'submissions', 'base.csv'))
+    y_test_pred = np.zeros((X_test.shape[0], len(CLASS_NAMES)))
+    y_test_pred[:, np.sort(list(set(y)))] = clf.predict_proba(X_test)
+    pandas.DataFrame(y_test_pred, index=testFnames).reset_index() \
+        .to_csv(os.path.join(DATA_DIR, 'submissions', 'base_%s.csv' % datetime.date.today().strftime('%b%d%Y')),
+                header = ['image'] + CLASS_NAMES, index=False)
